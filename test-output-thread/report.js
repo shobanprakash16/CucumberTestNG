b4089a9411fc9cc18243395a928033c48bee8911
shobanprakash16 <shobanprakash16@Gmail.com>
2025-08-21 22:38:52 +0530

    Delete test-output-thread directory

--- test-output-thread/report.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"8c3efabd-549d-4dd5-8faa-c7a17e3af217","feature":"This feature file is for practicing the API automation","scenario":"This is a API Automation Scenario","start":1755757287972,"group":1,"content":"","tags":"@apiautomation,","end":1755757289940,"className":"passed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
-});
